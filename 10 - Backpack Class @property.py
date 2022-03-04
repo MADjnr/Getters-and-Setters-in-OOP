202ab14a45b9d3a1853f2037fbaeab2222cc1fa8
Madu A. Daniel <77062412+MADjnr@users.noreply.github.com>
2022-03-04 15:28:08 -0500

    new code

--- 10 - Backpack Class @property.py
+++ 10 - Backpack Class @property.py
@@ -2,7 +2,7 @@
 Course: Python OOP - Object Oriented Programming for Beginners
 By: Estefania Cassingena Navone
 """
-
+'''
 class Backpack:
 
     def __init__(self):
@@ -28,3 +28,38 @@ print(my_backpack.items)
 
 my_backpack.items = "Hello, World!" # Invalid value.
 print(my_backpack.items)
+'''
+
+## Define a class for the backpack
+## Define the constructor but make the item non public
+## build the getter method using @property
+## build the setter method using @property_name.setter
+## check if the items is a list
+
+class Backpack:
+
+    def __init__(self):
+        self._items = []
+
+    @property
+    def items(self):
+        return self._items
+
+    ##building the setter
+    @items.setter
+    def items(self, new_items):
+        if isinstance(new_items, list) and len(new_items) <= 7:
+            self._items = new_items
+
+        else:
+            print('The items are invalid')
+
+my_backpack = Backpack()
+
+my_backpack.items = ['banana', 'oranges', 'mango']
+print(my_backpack.items[2])
+
+my_backpack.items.append('yoha')
+
+print(my_backpack.items)
+
--- 9 - Movie Class @property.py
+++ 9 - Movie Class @property.py
@@ -2,7 +2,7 @@
 Course: Python OOP - Object Oriented Programming for Beginners
 By: Estefania Cassingena Navone
 """
-
+'''
 class Movie:
 
     def __init__(self, title, rating):
@@ -31,3 +31,41 @@ print(favorite_movie.rating)
 
 favorite_movie.rating = -5.6 # Invalid value.
 print(favorite_movie.rating)
+'''
+
+# define a School class
+#define the instance attributes
+#make one of the instance attributes non-public
+# use a property decorator for getter
+# use a decorator for the setter
+
+#class School(object):
+
+
+class School:
+    def __init__(self, name, location, population):
+        self.name = name
+        self._location = location
+        self.population = population
+
+    @property
+    def location(self):
+        return self._location
+
+    @location.setter
+    def location(self, new_location):
+        if isinstance(new_location, str) and len(new_location) == len(self._location):
+            self._location = new_location
+
+        else:
+            print('Enter valid location')
+
+
+my_school = School('University of Nigeria', 'Enugu', 19000)
+
+print(my_school.name)
+print(my_school.location)
+
+my_school.location = 'opuka'
+
+print(my_school.location)
